a010335047f5339c4098ef0f5bf9f890cc40d539
Nykaela Dodson <nykaeladodson@Nykaelas-MBP.lan>
2022-08-02 17:07:49 -0700

    server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Example app listening on port 3000");
+});
