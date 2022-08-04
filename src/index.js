3d837457605af55930babab0890c3b7569c58c89
Nykaela Dodson <nykaeladodson@Nykaelas-MacBook-Pro.local>
2022-08-04 13:03:32 -0600

    morgan

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 app.use(cors());
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.send('hello world');
